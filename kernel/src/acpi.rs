9b41cc65bd7ada1353b13225676a3960725a98f8
GhostedGaming <epicrobloxgamer41@gmail.com>
2026-01-21 19:05:15 +0000

    Finished some bits

--- kernel/src/acpi.rs
+++ kernel/src/acpi.rs
@@ -11,14 +11,19 @@ struct RSDP {
     oem_id: [u8; 6],
     revision: u8,
     rsdt_ptr: u32,
+    xsdt_ptr: u64,
 }
 
 impl RSDP {
-    fn new() -> &'static Self {
+    fn get() -> &'static Self {
         unsafe {
-           let rsdp_res = RSDP_REQUEST.get_response().unwrap();
-            &*(rsdp_res.address() as *const RSDP)
+            let res = RSDP_REQUEST.get_response().unwrap();
+            &*(res.address() as *const RSDP)
         }
+    }
+
+    fn xsdt(&self) -> &'static XSDT {
+        unsafe { &*(self.xsdt_ptr as *const XSDT) }
     }
 }
 
@@ -41,11 +46,28 @@ struct XSDT {
 }
 
 impl XSDT {
-    fn entries(&self) -> &'static [u64] {
-        let count =
-            (self.header.length as usize - core::mem::size_of::<ACPISDTHeader>()) / 8;
-        let base = unsafe { (self as *const _ as *const u8)
-            .add(core::mem::size_of::<ACPISDTHeader>()) } as *const u64;
+    fn entries(&self) -> &[u64] {
+        let header_size = core::mem::size_of::<ACPISDTHeader>();
+        let count = (self.header.length as usize - header_size) / 8;
+        let base = unsafe {
+            (self as *const _ as *const u8).add(header_size) as *const u64
+        };
         unsafe { core::slice::from_raw_parts(base, count) }
     }
+
+    fn find_sdt(&self, sig: &[u8; 4]) -> Option<&'static ACPISDTHeader> {
+        for &entry in self.entries() {
+            let hdr = unsafe { &*(entry as *const ACPISDTHeader) };
+            if &hdr.signature == sig {
+                return Some(hdr);
+            }
+        }
+        None
+    }
+}
+
+pub fn find_acpi_table(sig: &[u8; 4]) -> Option<&'static ACPISDTHeader> {
+    let rsdp = RSDP::get();
+    let xsdt = rsdp.xsdt();
+    xsdt.find_sdt(sig)
 }
